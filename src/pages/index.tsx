89c0f7ce6d504bce46f9b3f922f00123aaa2ee56
guitavano <tavano62@gmail.com>
2022-05-01 23:08:58 -0300

    first steps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head'
 
 import { getPrismicClient } from '../services/prismic';
 
+import * as prismic from '@prismicio/client'
+
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Link from 'next/link'
 
 interface Post {
   uid?: string;
@@ -21,16 +25,55 @@ interface PostPagination {
 }
 
 interface HomeProps {
-  postsPagination: PostPagination;
+  postsResponse: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsResponse }: HomeProps) {
+  console.log(postsResponse)
+  return (
+    <>
+      <Head>
+        <title>Home Spacetraveling</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {
+            postsResponse.results.map(post => (
+              <Link href={`/posts/${post.uid}`} key={post.uid}>
+                <a>
+                  <strong>{post.data.title}</strong>
+                  <p>{post.data.subtitle}</p>
+                  <div className={styles.info}>
+                    <div>
+                      <p>{post.first_publication_date}</p>
+                    </div>
+                    <div>
+                      <p>{post.data.author}</p>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            ))
+          }
+        </div>
+      </main>
+    </>
+  )
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps = async () => {
+  const endpoint = process.env.PRISMIC_ENDPOINT;
 
-//   // TODO
-// };
+  const client = prismic.createClient(endpoint)
+
+  const postsResponse = await client.getByType('posts');
+
+  console.log(postsResponse)
+
+  return {
+    props: {
+      postsResponse
+    }
+  }
+
+};
